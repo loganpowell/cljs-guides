58a37fb3cb6864c260034324df58e843cb244231
loganpowell <loganp@tepper.cmu.edu>
2018-06-02 12:57:50 -0400

    alts! timeout and buffering intro

--- public/node/cljs-runtime/shadow.umd_helper.js
+++ public/node/cljs-runtime/shadow.umd_helper.js
@@ -2,7 +2,7 @@ goog.provide('shadow.umd_helper');
 goog.require('cljs.core');
 goog.require('guides.core');
 shadow.umd_helper.get_exports = (function shadow$umd_helper$get_exports(){
-var obj23640 = {"guides":guides.core.node};
-return obj23640;
+var obj23624 = {"guides":guides.core.node};
+return obj23624;
 });
 goog.exportSymbol('shadow.umd_helper.get_exports', shadow.umd_helper.get_exports);
